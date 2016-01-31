abf3c4dcfaa37778ac9847ee6294bfcc66688f23
Chris Katnic <chris.katnic@gmail.com>
2016-01-31 11:01:41 -0800

    Initial Commit

---
+++ TheBetterBoyfriend/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheBetterBoyfriend
+//
+//  Created by Christopher Katnic on 1/31/16.
+//  Copyright © 2016 Christopher Katnic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
